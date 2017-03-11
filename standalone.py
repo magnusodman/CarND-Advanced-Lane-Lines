771225a2fafb1c1ccd3a86825b8daa8692de264f
Magnus Ödman <magnus.odman@gmail.com>
2017-03-11 09:31:20 +0100

    Functional pipeline

--- standalone.py
+++ standalone.py
@@ -2,40 +2,52 @@ import numpy as np
 import cv2
 import glob
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((9*6,3), np.float32)
-objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+def calibrate_camera():
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((9*6,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('camera_cal/*.jpg')
+    images = glob.glob('camera_cal/*.jpg')
 
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-        imgpoints.append(corners)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
 
 
-img = cv2.imread("camera_cal/calibration1.jpg")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.imread("camera_cal/calibration1.jpg")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-print("Calibrated!")
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    print("Calibrated!")
+    return mtx, dist
 
+#####
+#   Calibrate camera and create undistort function
+####
+mtx, dist = calibrate_camera()
 def undistort(img):
     return cv2.undistort(img, mtx, dist, None, mtx)
 
+####
+##  Create perspective transform
+####
 def persp_trans(im):
     
     width = im.shape[1]
@@ -56,6 +68,9 @@ def persp_trans(im):
     warped = cv2.warpPerspective(im, M, (dst_width, dst_height), flags=cv2.INTER_LINEAR)
     return warped
 
+###
+#   Performs absolute sobel threshold on input image ing and returns a binary gradient image
+##
 def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     # Apply x or y gradient with the OpenCV Sobel() function
@@ -74,7 +89,10 @@ def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     # Return the result
     return binary_output
 
-def color_gradient(image, thresh=(0, 255)):
+##
+#   Returns the image of the s channel in a hls representation of input image "image" that meets the supplied threshold requirements.
+##
+def color_threshold_for_s_channnel(image, thresh=(0, 255)):
     #color gradient
     hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
     H = hls[:,:,0]
@@ -85,140 +103,88 @@ def color_gradient(image, thresh=(0, 255)):
     binary[(S > thresh[0]) & (S <= thresh[1])] = 1
     return binary
 
+####
+#   Returns the combined sobel and color gradients
+###
 def grad_col(image):
     gradx = abs_sobel_thresh(image, thresh=(30, 200))
     grady = abs_sobel_thresh(image, orient='y', thresh=(40, 200))
-    col_grad = color_gradient(image, thresh = (90, 255))
+    col_grad = color_threshold_for_s_channnel(image, thresh = (90, 255))
 
     combined = np.zeros_like(col_grad)
     combined[((gradx == 1) & (grady == 1)) | (col_grad == 1)] = 1
     return combined
 
-import numpy as np
-
-curved_lane = cv2.cvtColor(cv2.imread('test_images/test2.jpg'), cv2.COLOR_BGR2RGB)
-curved_lane = undistort(curved_lane)
-gradient_image = grad_col(curved_lane)
-corrected = persp_trans(gradient_image)
-
-"""
-plt.imshow(corrected, cmap="gray")
-plt.show()
-
-histogram = np.sum(corrected[corrected.shape[0]/2:,:], axis=0)
-
-plt.plot(histogram)
-plt.show()
-
-c_img = np.dstack((corrected, corrected, corrected))
-
-
-plt.imshow(c_img)
-plt.title("two")
-plt.show()
-"""
-
 
 ############################################
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
 
-binary_warped = corrected
-"""
-print(corrected.shape)
-plt.imshow(binary_warped, cmap='gray')
-plt.title("one")
-plt.show()
-"""
 
-# Assuming you have created a warped binary image called "binary_warped"
-# Take a histogram of the bottom half of the image
-histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
-# Create an output image to draw on and  visualize the result
-out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255.0
-# Find the peak of the left and right halves of the histogram
-# These will be the starting point for the left and right lines
-midpoint = np.int(histogram.shape[0]/2)
-leftx_base = np.argmax(histogram[:midpoint])
-rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+def detect_lines(binary_warped):
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255.0
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
 
-# Choose the number of sliding windows
-nwindows = 9
-# Set height of windows
-window_height = np.int(binary_warped.shape[0]/nwindows)
-# Identify the x and y positions of all nonzero pixels in the image
-nonzero = binary_warped.nonzero()
-nonzeroy = np.array(nonzero[0])
-nonzerox = np.array(nonzero[1])
-# Current positions to be updated for each window
-leftx_current = leftx_base
-rightx_current = rightx_base
-# Set the width of the windows +/- margin
-margin = 100
-# Set minimum number of pixels found to recenter window
-minpix = 50
-# Create empty lists to receive left and right lane pixel indices
-left_lane_inds = []
-right_lane_inds = []
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
 
-# Step through the windows one by one
-for window in range(nwindows):
-    # Identify window boundaries in x and y (and right and left)
-    win_y_low = binary_warped.shape[0] - (window+1)*window_height
-    win_y_high = binary_warped.shape[0] - window*window_height
-    win_xleft_low = leftx_current - margin
-    win_xleft_high = leftx_current + margin
-    win_xright_low = rightx_current - margin
-    win_xright_high = rightx_current + margin
-    # Draw the windows on the visualization image
-    cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
-    cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
-    # Identify the nonzero pixels in x and y within the window
-    good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
-    good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
-    # Append these indices to the lists
-    left_lane_inds.append(good_left_inds)
-    right_lane_inds.append(good_right_inds)
-    # If you found > minpix pixels, recenter next window on their mean position
-    if len(good_left_inds) > minpix:
-        leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-    if len(good_right_inds) > minpix:        
-        rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
 
-# Concatenate the arrays of indices
-left_lane_inds = np.concatenate(left_lane_inds)
-right_lane_inds = np.concatenate(right_lane_inds)
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
 
-# Extract left and right line pixel positions
-leftx = nonzerox[left_lane_inds]
-lefty = nonzeroy[left_lane_inds] 
-rightx = nonzerox[right_lane_inds]
-righty = nonzeroy[right_lane_inds] 
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+    return leftx, lefty, rightx, righty
 
-# Fit a second order polynomial to each
-left_fit = np.polyfit(lefty, leftx, 2)
-right_fit = np.polyfit(righty, rightx, 2)
-
-print(left_fit)
-print(right_fit)
-
-# Generate x and y values for plotting
-ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
-out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-
-#plt.imshow(out_img)
-"""
-plt.plot(left_fitx, ploty, color='red')
-plt.plot(right_fitx, ploty, color='blue')
-plt.xlim(0, 1280)
-plt.ylim(720, 0)
-plt.show()
-"""
 
 def matrixes():
     
@@ -241,7 +207,30 @@ def matrixes():
     return M, Minv
 
 
-def decorate_lane(Minv):
+def process_image_real(img):
+    #curved_lane = cv2.cvtColor(cv2.imread('test_images/test2.jpg'), cv2.COLOR_BGR2RGB)
+    img = undistort(img)
+    gradient_image = grad_col(img)
+    corrected = persp_trans(gradient_image)
+    binary_warped = corrected
+    # Fit a second order polynomial to each
+
+    leftx, lefty, rightx, righty = detect_lines(binary_warped)
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    #print(left_fit)
+    #print(right_fit)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    M, Minv = matrixes()
+    
+    #plt.imshow(result)
+    #plt.show()
     # Create an image to draw the lines on
     warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
@@ -257,11 +246,33 @@ def decorate_lane(Minv):
     # Warp the blank back to original image space using inverse perspective matrix (Minv)
     newwarp = cv2.warpPerspective(color_warp, Minv, (binary_warped.shape[1], binary_warped.shape[0])) 
     # Combine the result with the original image
-    result = cv2.addWeighted(curved_lane, 1, newwarp, 0.3, 0)
+    result = cv2.addWeighted(img, 1, newwarp, 0.3, 0)
     return result
+    
+#out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+#out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 
-M, Minv = matrixes()
-result = decorate_lane(Minv)
-plt.imshow(result)
+#plt.imshow(out_img)
+"""
+plt.plot(left_fitx, ploty, color='red')
+plt.plot(right_fitx, ploty, color='blue')
+plt.xlim(0, 1280)
+plt.ylim(720, 0)
 plt.show()
+"""
+def process_image(img):
+    #print("*", end='')
+    return img
+
+
+
+
+from moviepy.editor import VideoFileClip
+
+
+    
+    
+clip1 = VideoFileClip("challenge_video.mp4")
+output_video = clip1.fl_image(process_image_real)
+output_video.write_videofile("challenge_output.mp4", audio=False)
 
